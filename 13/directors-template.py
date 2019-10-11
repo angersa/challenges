64a96edf7430f49663122bc4c4715e38174ca0b5
Annie Angers <aangers@mac.com>
2019-10-11 15:58:56 -0400

    started challenge 13

--- 13/directors-template.py
+++ 13/directors-template.py
@@ -1,7 +1,7 @@
 import csv
-from collections import defaultdict, namedtuple
+from collections import defaultdict, namedtuple, Counter
 
-MOVIE_DATA = 'movie_metadata.csv'
+MOVIE_DATA = '/Users/aa/GitHub/challenges/13/movie_metadata.csv'
 NUM_TOP_DIRECTORS = 20
 MIN_MOVIES = 4
 MIN_YEAR = 1960
@@ -9,15 +9,39 @@ MIN_YEAR = 1960
 Movie = namedtuple('Movie', 'title year score')
 
 
-def get_movies_by_director():
+def get_movies_by_director(data=MOVIE_DATA):
     '''Extracts all movies from csv and stores them in a dictionary
     where keys are directors, and values is a list of movies (named tuples)'''
-    pass
+    directors = defaultdict(list)
+    with open(data) as f:
+        for line in csv.DictReader(f):
+            try:
+                director = line['director_name']
+                movie = line['movie_title'].replace('\xa0', '')
+                year = int(line['title_year'])
+                score = float(line['imdb_score'])
+            except ValueError:
+                continue
+
+            m = Movie(title=movie, year=year, score=score)
+            if m.year >= MIN_YEAR:
+                directors[director].append(m)
+
+    return directors
 
 
 def get_average_scores(directors):
     '''Filter directors with < MIN_MOVIES and calculate averge score'''
-    pass
+    cnt = Counter()
+    less_than_min = []
+    for director, movies in directors.items():
+        cnt[director] += len(movies)
+        if cnt[director] < MIN_MOVIES:
+            less_than_min.append(director)
+    for director in less_than_min:
+        del directors[director]
+
+    return directors
 
 
 def _calc_mean(movies):
@@ -40,6 +64,8 @@ def main():
     directors = get_movies_by_director()
     directors = get_average_scores(directors)
     print_results(directors)
+
+
 
 
 if __name__ == '__main__':
